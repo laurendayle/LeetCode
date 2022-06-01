5d06dd8c1b21969bbc24f931544ae01aa6b42fd7
Lauren Anderson <laurendayle.anderson@gmail.com>
2022-06-01 17:03:53 -0400

    Time: 92 ms (49.74%), Space: 43.6 MB (73.58%) - LeetHub

--- 169-majority-element/169-majority-element.js
+++ 169-majority-element/169-majority-element.js
@@ -3,19 +3,19 @@
  * @return {number}
  */
 var majorityElement = function(nums) {
-    var counts = {};
-    for (var i = 0; i < nums.length; i++) {
-        if (counts[nums[i]] === undefined) {
-            counts[nums[i]] = 1;
-        } else {
-            counts[nums[i]]++;
-        }
+
+  const counts = nums.reduce((acc, num) => {
+    if (acc[num] === undefined) {
+      acc[num] = 1;
+    } else {
+      acc[num]++;
     }
-    
-    for (var key in counts) {
-        if (counts[key] > (nums.length / 2)) {
-            return key;
-        }
+    return acc;
+  }, {});
+  for (const key in counts) {
+    if (counts[key] > nums.length / 2) {
+      return key;
     }
+  }
     
 };
