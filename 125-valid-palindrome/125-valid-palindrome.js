d9274f7113ef707a99588c379f1920ce699f48e6
Lauren Anderson <laurendayle.anderson@gmail.com>
2022-06-01 13:36:16 -0400

    Time: 110 ms (37.68%), Space: 45.9 MB (53.07%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.js
+++ 125-valid-palindrome/125-valid-palindrome.js
@@ -6,16 +6,3 @@ var isPalindrome = function(s) {
   s = s.replace(/[^A-Za-z0-9]/g, '').toLowerCase();
   return s.split('').reverse().join('') === s;
 };
-
-// var isPalindrome = function(s) {
-//     // remove spaces and any special string
-//     s = s.replace(/[^A-Za-z0-9]/g,'').toLowerCase();
-//     let left = 0;
-//     let right = s.length-1;
-//     while(left <= right) {
-//         if(s[left] !== s[right] ) return false;
-//         left++;
-//         right--;
-//     }
-//     return true;
-// };``
