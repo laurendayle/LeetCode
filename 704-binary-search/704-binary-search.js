c4d9e19af4a562f471e0bf8532c29c9e9db79379
Lauren Anderson <laurendayle.anderson@gmail.com>
2022-06-02 12:30:28 -0400

    Time: 116 ms (11.33%), Space: 45.3 MB (32.78%) - LeetHub

--- 704-binary-search/704-binary-search.js
+++ 704-binary-search/704-binary-search.js
@@ -4,18 +4,19 @@
  * @return {number}
  */
 const search = (nums, target) => {
-
-  let start = 0;
-  let end = nums.length - 1;
-
+  
+  let start = 0; 
+  let end = nums.length - 1; 
+  
   while (start <= end) {
     let mid = Math.floor((start + end) / 2);
+    
     if (nums[mid] === target) {
       return mid;
-    } else if (nums[mid] > target) {
-      end = mid - 1;
-    } else if (nums[mid] < target) {
+    } else if (target > nums[mid]) {
       start = mid + 1;
+    } else if (target < nums[mid]) {
+      end = mid - 1;
     }
   }
   return -1;
