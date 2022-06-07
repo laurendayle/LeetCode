46ec5d1e59f34ecdebc1159351899b0e39e3d19f
Lauren Anderson <laurendayle.anderson@gmail.com>
2022-06-07 11:59:28 -0400

    Time: 123 ms (37.93%), Space: 48.6 MB (29.88%) - LeetHub

---
+++ 554-brick-wall/554-brick-wall.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[][]} wall
+ * @return {number}
+ */
+var leastBricks = function(wall) {
+
+  let freq = new Map();
+  let best = 0;
+  for (let i = 0; i < wall.length; i++) {
+    let row = wall[i];
+    let rowSum = row[0];
+    for (let j = 1; j < row.length; j++) {
+      freq.set(rowSum, (freq.get(rowSum) || 0) + 1);
+      rowSum += row[j];
+    }
+  }
+  for (let [k, v] of freq) {
+    if (v > best) {
+      best = v;
+    }
+  }
+  return wall.length - best;
+}
