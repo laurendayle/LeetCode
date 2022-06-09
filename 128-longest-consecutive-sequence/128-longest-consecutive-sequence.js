59f7042b6e8a1b76bfbab7a93df18a1913f620f0
Lauren Anderson <laurendayle.anderson@gmail.com>
2022-06-09 12:53:24 -0400

    Time: 128 ms (74.16%), Space: 50.3 MB (81.80%) - LeetHub

---
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var longestConsecutive = function(nums) {
+  if (nums.length === 0) {
+    return 0;
+  }
+
+  nums.sort((a, b) => a - b);
+
+  let longest = 1;
+  let prev = nums[0];
+  let curr = 1;
+  for (let i = 1; i < nums.length; i++) {
+    debugger;
+    if (nums[i] === prev + 1) {
+      curr++;
+    } else if (nums[i] !== prev) {
+      curr = 1;
+    }
+    prev = nums[i];
+    longest = Math.max(longest, curr);
+  }
+  return longest;
+}
