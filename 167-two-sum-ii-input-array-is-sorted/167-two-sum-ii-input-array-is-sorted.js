f78083f21ad0a52b7ce5dc477079d63b0bb46bd6
Lauren Anderson <laurendayle.anderson@gmail.com>
2022-06-09 13:35:23 -0400

    Time: 98 ms (32.92%), Space: 42.9 MB (65.78%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.js
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.js
@@ -4,18 +4,17 @@
  * @return {number[]}
  */
 var twoSum = function(numbers, target) {
+
   let p1 = 0;
   let p2 = numbers.length - 1;
-  let stop = false;
 
-  while (!stop) {
-    if (numbers[p1] + numbers[p2] > target) {
+  while (p1 !== p2) {
+    if (numbers[p1] + numbers[p2] === target) {
+      return [p1 + 1, p2 + 1];
+    } else if (numbers[p1] + numbers[p2] > target) {
       p2--;
-    } else if (numbers[p1] + numbers[p2] < target) {
-      p1++;
     } else {
-      stop = true;
+      p1++;
     }
   }
-  return [p1 + 1, p2 + 1];
-};
+}
