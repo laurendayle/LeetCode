7587309e40e2b017e80cffb8f7f95ee74b815f7b
Lauren Anderson <laurendayle.anderson@gmail.com>
2022-06-10 16:02:57 -0400

    Time: 99 ms (32.20%), Space: 43.9 MB (86.91%) - LeetHub

--- 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.js
+++ 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.js
@@ -10,27 +10,30 @@
  * @param {TreeNode} root
  * @return {number[][]}
  */
-
-const levelOrder = (root) => {
-  let result = [];
+var levelOrder = function(root) {
   
   if (!root) {
-    return result;
+    return [];
   }
-  recurse(root, 0, result);
-  return result;
-}
-
-const recurse = (node, level, result) => {
-  if (result.length === level) {
-    result.push([]);
+  const queue = [root];
+  const output = [];
+  while (queue.length) {
+    // remove all current nodes in the queue and add each 
+    const len = queue.length; 
+    const row = [];
+    for (let i = 0; i < len; i++) {
+      const curr = queue.shift();
+      if (curr.left) {
+        queue.push(curr.left);
+      }
+      if (curr.right) {
+        queue.push(curr.right);
+      }
+      // push current node val to the row array 
+      row.push(curr.val);
+    }
+    // push the current row array into the output array 
+    output.push(row);
   }
-  result[level].push(node.val);
-  
-  if (node.left) {
-    recurse(node.left, level + 1, result);
-  }
-  if (node.right) {
-    recurse(node.right, level + 1, result);
-  }
-}
+  return output; 
+};
