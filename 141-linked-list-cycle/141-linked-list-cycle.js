f5e771b4f0da1d862f675af92e4a1b269f9e108d
Lauren Anderson <laurendayle.anderson@gmail.com>
2022-06-12 17:40:53 -0400

    Time: 112 ms (33.01%), Space: 44.8 MB (74.59%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.js
+++ 141-linked-list-cycle/141-linked-list-cycle.js
@@ -11,17 +11,20 @@
  * @return {boolean}
  */
 var hasCycle = function(head) {
+  
   if (head === null) {
     return false;
   }
-  let slow = head;
-  let fast = head.next;
+  
+  let fast = head.next,
+      slow = head;
+  
   while (slow !== fast) {
-    if (fast === null || fast.next === null) {
+    if (fast === null || fast.next == null) {
       return false;
     }
     slow = slow.next;
     fast = fast.next.next;
   }
-  return true; 
+  return true;
 };
