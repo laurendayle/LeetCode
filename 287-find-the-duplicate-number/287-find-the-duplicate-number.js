94462981e621aa22995fdf2bdb7c17487460df70
Lauren Anderson <laurendayle.anderson@gmail.com>
2022-06-12 17:43:26 -0400

    Time: 112 ms (68.79%), Space: 62.2 MB (17.59%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.js
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.js
@@ -3,17 +3,13 @@
  * @return {number}
  */
 var findDuplicate = function(nums) {
-    
   
+  let hash = new Map();
   for (var i = 0; i < nums.length; i++) {
-    var pointer = i + 1;
-    
-    while (pointer < nums.length) {
-      if (nums[i] === nums[pointer]) {
-        return nums[i];
-      } else {
-        pointer++;
-      }
+    if (hash.get(nums[i]) === undefined) {
+      hash.set(nums[i], 0);
+    } else {
+      return nums[i];
     }
   }
 };
