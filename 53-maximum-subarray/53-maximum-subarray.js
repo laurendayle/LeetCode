8afbf1bd10e848d51b36882f3e068102bf9172ff
Lauren Anderson <laurendayle.anderson@gmail.com>
2022-06-12 16:36:52 -0400

    Time: 93 ms (77.35%), Space: 50 MB (92.21%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.js
+++ 53-maximum-subarray/53-maximum-subarray.js
@@ -2,18 +2,14 @@
  * @param {number[]} nums
  * @return {number}
  */
-const maxSubArray = (nums) => {
-  // initialize a currentSubarray variable and set to nums[0]
-  let currentSub= nums[0];
-  // initialize a maxSubarray variable and set to nums[0]
-  let maxSub = nums[0];
-  // iterate through the array starting with the 2nd element
+var maxSubArray = function(nums) {
+  let current = nums[0],
+      max = nums[0];
+  
   for (var i = 1; i < nums.length; i++) {
     let num = nums[i];
-    // determine if num is worth keeping and add to currentSubarray 
-    currentSub= Math.max(num, currentSub + num);
-    // if currentSubarray is greater than maxSubarray, set max to current 
-    maxSub = Math.max(maxSub, currentSub);
+    current = Math.max(num, current + num);
+    max = Math.max(max, current);
   }
-  return maxSub;
-}
+  return max;
+};
