c5910c7293a617f8f92830d4aedeea14efd0e580
Lauren Anderson <laurendayle.anderson@gmail.com>
2022-06-12 18:40:45 -0400

    Time: 75 ms (55.19%), Space: 41.7 MB (70.97%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.js
+++ 70-climbing-stairs/70-climbing-stairs.js
@@ -2,16 +2,21 @@
  * @param {number} n
  * @return {number}
  */
-var climbStairs = function(n, memo = {}) {
-  if (memo[n]) {
-    return memo[n];
+var climbStairs = function(n) {
+  let memo = new Array(n + 1).fill(0);
+  return climb_stairs(0, n, memo);
+};
+
+var climb_stairs = function(i, n, memo) {
+  if (i > n) {
+    return 0;
   }
-  if (n === 1) {
+  if (i === n) {
     return 1;
   }
-  if (n === 2) {
-    return 2;
+  if (memo[i] > 0) {
+    return memo[i];
   }
-  memo[n] = climbStairs(n - 1, memo) + climbStairs(n - 2, memo);
-  return memo[n];
+  memo[i] = climb_stairs(i + 1, n, memo) + climb_stairs(i + 2, n, memo);
+  return memo[i];
 }
