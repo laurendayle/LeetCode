730abb36d5960c629cc80db25badee65b0f11c8c
Lauren Anderson <laurendayle.anderson@gmail.com>
2022-06-13 17:39:34 -0400

    Time: 105 ms, Memory: 45 MB - LeetHub

--- symmetric-tree/symmetric-tree.js
+++ symmetric-tree/symmetric-tree.js
@@ -11,20 +11,20 @@
  * @return {boolean}
  */
 var isSymmetric = function(root) {
-  if (!root) {
-    return true;
+  if (root === null) {
+    return true; 
   }
   return isMirror(root.left, root.right);
-};
+}
 
 var isMirror = function(tree1, tree2) {
-  if (!tree1 && !tree2) {
-    return true;
-  } 
-  if (!tree1 || !tree2) {
-    return false;
+  if (tree1 === null && tree2 === null) {
+    return true; 
+  }
+  if (tree1 === null || tree2 === null) {
+    return false; 
   }
   return (tree1.val === tree2.val)
     && isMirror(tree1.left, tree2.right)
-    && isMirror(tree1.right, tree2.left);
+    && isMirror(tree1.right, tree2.left); 
 }
