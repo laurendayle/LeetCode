bae2628345125d8a886fd6a3bee186f02071a5a9
Lauren Anderson <laurendayle.anderson@gmail.com>
2022-06-14 16:29:51 -0400

    Time: 92 ms (29.80%), Space: 42.5 MB (24.64%) - LeetHub

--- 100-same-tree/100-same-tree.js
+++ 100-same-tree/100-same-tree.js
@@ -12,13 +12,9 @@
  * @return {boolean}
  */
 var isSameTree = function(p, q) {
-  if (p === null && q === null) {
-    return true;
-  } else if (p === null || q === null) {
-    return false;
-  }
-  if (p.val !== q.val) {
-    return false;
-  }
-  return isSameTree(p.right, q.right) && isSameTree(p.left, q.left);
+  if (p === null && q === null) return true; 
+  if (p === null || q === null) return false;
+  return (p.val === q.val) 
+    && isSameTree(p.left, q.left) 
+    && isSameTree(p.right, q.right); 
 };
