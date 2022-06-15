31e6532be646f2e64168dbdfe6df7dc362d5714b
Lauren Anderson <laurendayle.anderson@gmail.com>
2022-06-15 11:22:59 -0400

    Time: 61 ms (98.53%), Space: 42.9 MB (38.85%) - LeetHub

--- 1-two-sum/1-two-sum.js
+++ 1-two-sum/1-two-sum.js
@@ -2,20 +2,14 @@
  * @param {number[]} nums
  * @param {number} target
  * @return {number[]}
- 
- 
- Given an array of integers and an integer target, return the indices of the two elements that add up to target 
- 
- I: array of integers
- O: array of two indices 
- C: cannot use same element twice 
- E: empty array, 1 or two elements 
  */
 var twoSum = function(nums, target) {
-  for (let i = 0; i < nums.length; i++) {
-    let index = nums.indexOf(target - nums[i]);
-    if (index !== -1 && index !== i) {
-      return [i, index];
+  let hash = new Map();
+  for (var i = 0; i < nums.length; i++) {
+    let complement = target - nums[i];
+    if (hash.has(complement)) {
+      return [hash.get(complement), i];
     }
+    hash.set(nums[i], i); 
   }
 };
