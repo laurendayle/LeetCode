d739b62dc4126c339d9453505d321a6c9272a5ea
Lauren Anderson <laurendayle.anderson@gmail.com>
2022-06-15 12:22:34 -0400

    Time: 62 ms (90.03%), Space: 42.3 MB (43.56%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.js
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.js
@@ -10,16 +10,33 @@
  * @param {TreeNode} root
  * @return {number[]}
  */
-var inorderTraversal = function(root, result = []) {
-  if (root === null) {
-    return result;
-  } 
-  
-  inorderTraversal(root.left, result);
-  
-  result.push(root.val);
-  
-  inorderTraversal(root.right, result);
-  
-  return result; 
+var inorderTraversal = function(root) {
+  return traversal(root, []);
 };
+
+var traversal = function(root, order) {
+  if (!root) return [];
+  
+  if (root.left) order = traversal(root.left, order);
+  order.push(root.val);
+  if (root.right) order = traversal(root.right, order);
+  
+  return order; 
+}
+
+var inorderTraversal = function(root) {
+  let result = [],
+      stack = [],
+      current = root;
+  
+  while (current !== null || stack.length) {
+    while (current !== null) {
+      stack.push(current);
+      current = current.left;
+    }
+    current = stack.pop();
+    result.push(current.val);
+    current = current.right;
+  }
+  return result; 
+}
