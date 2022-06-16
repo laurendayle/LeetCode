61d6e86ec84676aa772f378aa359b0b7d5b5936d
Lauren Anderson <laurendayle.anderson@gmail.com>
2022-06-16 16:12:34 -0400

    Time: 80 ms (91.13%), Space: 49.3 MB (78.60%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.js
+++ 11-container-with-most-water/11-container-with-most-water.js
@@ -5,16 +5,16 @@
 var maxArea = function(height) {
 
   let max = 0,
-      current = 0,
-      left = 0,
-      right = height.length - 1;
-  while (right > left) {
+      right = height.length - 1,
+      left = 0;
+
+  for (var i = 0; i < height.length; i++) {
     let width = right - left; 
-    max = Math.max(max, Math.min(height[left], height[right]) * width); 
-    if (height[left] <= height[right]) {
-      left++; 
+    max = Math.max(max, Math.min(height[left], height[right]) * width);
+    if (height[right] > height[left]) {
+      left++;
     } else {
-      right--; 
+      right--;
     }
   }
   return max; 
