9fe4410dad45b56a11d9c924dc4fc2254347fa49
Lauren Anderson <laurendayle.anderson@gmail.com>
2022-06-20 14:25:07 -0400

    Time: 125 ms (55.84%), Space: 50.9 MB (57.22%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.js
+++ 217-contains-duplicate/217-contains-duplicate.js
@@ -3,15 +3,12 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-  
-  
   let hash = new Map();
   for (var i = 0; i < nums.length; i++) {
-    if (!hash.has(nums[i])) {
-      hash.set(nums[i], 1);
-    } else {
+    if (hash.has(nums[i])) {
       return true; 
     }
+    hash.set(nums[i]);
   }
   return false; 
 };
