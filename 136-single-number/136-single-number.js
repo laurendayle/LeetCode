ea3a4739c241a0e797c8a3c588ce8639214fc320
Lauren Anderson <laurendayle.anderson@gmail.com>
2022-06-20 13:54:37 -0400

    Time: 126 ms (32.03%), Space: 49 MB (10.51%) - LeetHub

---
+++ 136-single-number/136-single-number.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = nums => {
+
+  let hash = new Map();
+  for (var i = 0; i < nums.length; i++) {
+    if (!hash.has(nums[i])) {
+      hash.set(nums[i], 1);
+    } else {
+      hash.set(nums[i], hash.get(nums[i]) + 1);
+    }
+  }
+  for (var [k, v] of hash) {
+    if (v === 1) {
+      return k;
+    }
+  }
+}
