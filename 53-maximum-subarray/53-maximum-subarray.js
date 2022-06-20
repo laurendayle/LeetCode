c1e1c9233316f7e4db5549fb3d0e69b119a03d54
Lauren Anderson <laurendayle.anderson@gmail.com>
2022-06-20 14:13:35 -0400

    Time: 144 ms (17.63%), Space: 50.6 MB (24.87%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.js
+++ 53-maximum-subarray/53-maximum-subarray.js
@@ -2,15 +2,15 @@
  * @param {number[]} nums
  * @return {number}
  */
-var maxSubArray = function(nums) {
-  let current = nums[0],
-      max = nums[0];
-  
+var maxSubArray = nums => {
+
+  let curr = nums[0];
+  let max = nums[0];
+
   for (var i = 1; i < nums.length; i++) {
-    let num = nums[i]; 
-    
-    current = Math.max(num, current + num); 
-    max = Math.max(current, max);
+    let num = nums[i];
+    curr = Math.max(curr + num, num);
+    max = Math.max(max, curr);
   }
-  return max; 
-};
+  return max;
+}
