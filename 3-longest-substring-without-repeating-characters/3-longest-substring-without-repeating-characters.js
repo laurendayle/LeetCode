4e039b9eec11ad8a436303d4b47877891fcdd55a
Lauren Anderson <laurendayle.anderson@gmail.com>
2022-06-20 14:50:23 -0400

    Time: 136 ms (57.27%), Space: 45 MB (89.59%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.js
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.js
@@ -2,18 +2,17 @@
  * @param {string} s
  * @return {number}
  */
-var lengthOfLongestSubstring = function(str) {
+var lengthOfLongestSubstring = function(s) {
+
+  let result = 0,
+      hash = new Map();
   
-  let n = str.length,
-      result = 0,
-      map = new Map();
-  
-  for (var j = 0, i = 0; j < n; j++) {
-    if (map.has(str[j])) {
-      i = Math.max(map.get(str[j]), i);
+  for (var j = 0, i = 0; j < s.length; j++) {
+    if (hash.has(s[j])) {
+      i = Math.max(hash.get(s[j]), i);
     }
     result = Math.max(result, j - i + 1);
-    map.set(str[j], j + 1); 
+    hash.set(s[j], j + 1);
   }
   return result; 
-}
+};
