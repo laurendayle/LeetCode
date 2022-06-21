6e5134418236522946bfda563c864c49d89d1edb
Lauren Anderson <laurendayle.anderson@gmail.com>
2022-06-21 18:39:30 -0400

    Time: 96 ms (37.51%), Space: 42.7 MB (64.77%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.js
+++ 22-generate-parentheses/22-generate-parentheses.js
@@ -3,29 +3,24 @@
  * @return {string[]}
  */
 var generateParenthesis = function(n) {
-  const result = []
+    let result = [];
 
-  var dfs = (i, n, slate, open, closed) => {
-    if (open > n) {
-      return;
-    }
-    if (closed > open) {
-      return;
-    }
-    if (i === n * 2) { // base case
+  const dfs = (n, i, slate, open, closed) => {
+    if (open > n) return;
+    if (closed > open) return;
+    if (i === n * 2) {
       result.push(slate.join(''));
       return;
     }
-    // dfs recursive case
-    // add open paren
+
     slate.push('(');
-    dfs(i + 1, n, slate, open + 1, closed);
+    dfs(n, i + 1, slate, open + 1, closed);
     slate.pop();
-    // add closed paren
     slate.push(')');
-    dfs(i + 1, n, slate, open, closed + 1);
+    dfs(n, i + 1, slate, open, closed + 1); 
     slate.pop();
   }
-  dfs(0, n, [], 0, 0);
-  return result;
-}
+
+  dfs(n, 0, [], 0, 0);
+  return result; 
+};
