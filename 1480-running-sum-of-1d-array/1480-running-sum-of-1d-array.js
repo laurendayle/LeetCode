6311199f00461c80d45ac9aacb27463187b5629b
Lauren Anderson <laurendayle.anderson@gmail.com>
2022-06-21 18:15:44 -0400

    Time: 68 ms (86.47%), Space: 42.2 MB (70.97%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.js
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.js
@@ -2,11 +2,10 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-var runningSum = function(nums) {
-    let sum = 0;
-    let result = []
-    nums.forEach(num => {
-        result.push(sum += num); 
-    })
-    return result;
-};
+var runningSum = nums => {
+  
+  for (var i = 1; i < nums.length; i++) {
+    nums[i] = nums[i - 1] + nums[i];
+  }
+  return nums;
+}
