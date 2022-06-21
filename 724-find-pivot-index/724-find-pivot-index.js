0af59902133decbffff72044c19521f30f50f49a
Lauren Anderson <laurendayle.anderson@gmail.com>
2022-06-21 18:25:32 -0400

    Time: 129 ms (41.66%), Space: 44.8 MB (38.83%) - LeetHub

---
+++ 724-find-pivot-index/724-find-pivot-index.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var pivotIndex = nums => {
+
+  let sum = 0,
+      leftSum = 0;
+
+  for (var i = 0; i < nums.length; i++) {
+    sum += nums[i];
+  }
+
+  for (var i = 0; i < nums.length; i++) { 
+    if (leftSum === sum - leftSum - nums[i]) {
+      return i;
+    }
+    leftSum += nums[i]; 
+  }
+  return -1; 
+}
