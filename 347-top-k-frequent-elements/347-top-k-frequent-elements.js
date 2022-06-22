c875da464249171bb4c29696f0cfaa078403ed11
Lauren Anderson <laurendayle.anderson@gmail.com>
2022-06-22 12:42:51 -0400

    Time: 154 ms (17.86%), Space: 47.7 MB (27.77%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.js
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.js
@@ -3,29 +3,35 @@
  * @param {number} k
  * @return {number[]}
  */
+var topKFrequent = (nums, k) => {
 
-var topKFrequent = function(nums, k) {
-    let counts = new Map();
-    let buckets = [];
-    for (let i = 0; i <= nums.length; i++)
-        buckets.push([]);
-    
-    // count frequent of the elements
-    for (let num of nums) {
-        if (counts.has(num)) {
-            counts.set(num, counts.get(num) + 1);
-        } else {
-            counts.set(num, 1);
-        }
-    } 
-    // put them into buckets by frequent
-    for (let [key, value] of counts) {
-        buckets[value].push(key);
+  let counts = new Map();
+  let buckets = [];
+
+  for (var i = 0; i <= nums.length; i++) {
+    buckets.push([]);
+  }
+
+  // count frequencies of the elements
+  for (let num of nums) {
+    if (counts.has(num)) {
+      counts.set(num, counts.get(num) + 1);
+    } else {
+      counts.set(num, 1);
     }
-    // fetch the larget frequest bucket first, until reach k
-    let ans = [];
-    for (let i = buckets.length - 1; i > 0 && ans.length < k; i--) {
-        if (buckets[i] !== null) ans.push(...buckets[i]);
+  }
+
+  // put them into buckets by frequency 
+  for (let [key, value] of counts) {
+    buckets[value].push(key);
+  }
+
+  // get the largest bucket until reaching k
+  let result = [];
+  for (let i = buckets.length - 1; i > 0 && result.length < k; i--) {
+    if (buckets[i] !== null) {
+      result.push(...buckets[i]);
     }
-    return ans;
-};
+  }
+  return result; 
+}
