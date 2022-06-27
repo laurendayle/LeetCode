50d5cd13f90924b682dbbd94bded0a4808e8be4d
Lauren Anderson <laurendayle.anderson@gmail.com>
2022-06-27 18:23:09 -0400

    Time: 112 ms (55.02%), Space: 44.5 MB (41.47%) - LeetHub

--- 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.js
+++ 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.js
@@ -4,16 +4,13 @@
  */
 var smallerNumbersThanCurrent = nums => {
 
-  let result = [];
-  var count = 0;
-  for (var i = 0; i < nums.length; i++) {
-    for (var j = 0; j < nums.length; j++) {
-      if (nums[i] > nums[j]) {
-        count++;
-      }
-    }
-    result.push(count);
-    count = 0;
+  const result = [];
+  const numsArr = [...nums];
+  numsArr.sort((a, b) => a - b);
+  
+  for (let i of nums) {
+    const index = numsArr.indexOf(i);
+    result.push(index);
   }
   return result;
 }
