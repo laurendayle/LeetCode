f1063ddd53dc18b431bec57ac5355ce585580772
Lauren Anderson <laurendayle.anderson@gmail.com>
2022-06-27 19:29:36 -0400

    Time: 76 ms (77.75%), Space: 43.5 MB (59.49%) - LeetHub

--- 78-subsets/78-subsets.js
+++ 78-subsets/78-subsets.js
@@ -3,20 +3,20 @@
  * @return {number[][]}
  */
 var subsets = nums => {
-  let result = [];
-  
-  const dfs = (i, nums, slate) => {
-    if (i === nums.length) {
-      result.push(slate.slice());
-      return;
-    }
-    
-    dfs(i + 1, nums, slate);
-    
-    slate.push(nums[i]);
-    dfs(i + 1, nums, slate);
-    slate.pop();
-  }
-  dfs(0, nums, []);
+  var result = [];
+  helper(nums, result, [], 0);
   return result;
+}
+
+var helper = (nums, result, arr, start) => {
+  var len = nums.length;
+
+  result.push(Array.from(arr));
+  if (start === len) return;
+
+  for (var i = start; i < len; i++) {
+    arr.push(nums[i]);
+    helper(nums, result, arr, i + 1);
+    arr.pop();
+  }
 }
