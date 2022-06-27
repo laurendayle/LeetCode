6782dfe7c1fbc149ae49fea606d747ea6a958aba
Lauren Anderson <laurendayle.anderson@gmail.com>
2022-06-27 19:41:23 -0400

    Time: 111 ms (89.95%), Space: 56.2 MB (17.17%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.js
+++ 238-product-of-array-except-self/238-product-of-array-except-self.js
@@ -3,19 +3,17 @@
  * @return {number[]}
  */
 var productExceptSelf = function(nums) {
-  
   let result = [],
       product = 1;
   
-  for (let i = 0; i < nums.length; i++) {
-    result[i] = product;
+  for (var i = 0; i < nums.length; i++) {
+    result[i] = product; 
     product *= nums[i];
   }
   product = 1;
-  
-  for (let i = nums.length - 1; i >= 0; i--) {
+  for (var i = nums.length -1; i >= 0; i--) {
     result[i] *= product;
     product *= nums[i];
   }
-  return result; 
+  return result;
 };
