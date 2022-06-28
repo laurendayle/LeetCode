c952bf163dd0fe888782f5051a89146606e6fdac
Lauren Anderson <laurendayle.anderson@gmail.com>
2022-06-28 17:47:46 -0400

    Time: 89 ms, Memory: 45.4 MB - LeetHub

--- maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.js
+++ maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.js
@@ -10,20 +10,8 @@
  * @param {TreeNode} root
  * @return {number}
  */
-/*
-1. return 0 if root is null                 // return 0 for null node
-2. left_depth = maximum_depth(root.left)
-3. right_depth = maximum_depth(root.right)
-4. return max(left_depth, right_depth) + 1  // return depth of the subtree rooted at root
-*/
+var maxDepth = root => {
+  if (!root) return 0;
 
-var maxDepth = function(root) {
-   
-  if (root === null) {
-    return 0;
-  }
-  let leftDepth = maxDepth(root.left);
-  let rightDepth = maxDepth(root.right);
-  return Math.max(leftDepth, rightDepth) + 1;
-  
-};
+  return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1; 
+}
