87c0a4dd5e8198892fd1a57ae9a8dae45015ed22
Lauren Anderson <laurendayle.anderson@gmail.com>
2022-07-12 10:44:42 -0400

    Time: 148 ms (88.98%), Space: 49.7 MB (78.07%) - LeetHub

--- 703-kth-largest-element-in-a-stream/703-kth-largest-element-in-a-stream.js
+++ 703-kth-largest-element-in-a-stream/703-kth-largest-element-in-a-stream.js
@@ -2,120 +2,27 @@
  * @param {number} k
  * @param {number[]} nums
  */
-function MinHeap() {
-  this.arr = [];
+var KthLargest = function(k, nums) {
+  this.values = nums.sort((a, b) => a - b);
+  this.k = k;
 }
 
-MinHeap.prototype.insert = function (item) {
-  this.arr.push(item);
-
-  // place item in correct position
-  // get indexes: curr and its parent
-  let curr = this.arr.length - 1,
-    parent = Math.floor((curr - 1) / 2);
-
-  // if the item is smaller than its parent
-  while (this.arr[curr] < this.arr[parent]) {
-    [this.arr[curr], this.arr[parent]] = [this.arr[parent], this.arr[curr]]; // swap
-
-    // reset parent and curr
-    curr = parent;
-    parent = Math.floor((curr - 1) / 2);
-    if (parent < 0) {
-      // if already top element
-      break;
+KthLargest.prototype.add = function(val) {
+  // search for a place to put val using binary search 
+  let left = 0, right = this.values.length - 1;
+  while (left < right) {
+    let mid = left + Math.floor((right + 1 - left) / 2);
+    if (val < this.values[mid]) {
+      right = mid - 1; 
+    } else {
+      left = mid; 
     }
   }
-};
-/**
- * Remove the minimum element from the heap
- * @returns {integer}
- */
-MinHeap.prototype.extractMin = function () {
-  // swap the first and last element and pop the last element off
-  [this.arr[0], this.arr[this.arr.length - 1]] = [
-    this.arr[this.arr.length - 1],
-    this.arr[0],
-  ];
-  const minVal = this.arr.pop();
 
-  // find the correct position
-  let curr = 0,
-    left = curr * 2 + 1,
-    right = curr * 2 + 2;
-
-  // calculate min of left and right
-  // if arr[right] does not exist, then left is the min
-  let min;
-  if (right >= this.arr.length) {
-    min = left;
-  } else if (left >= this.arr.length) {
-    // if arr[left] does not exist, set null
-    min = null;
-  } else {
-    min = this.arr[left] < this.arr[right] ? left : right;
-  }
-  // if the curr is greater than the min of child, swap
-  while (this.arr[curr] > this.arr[min] && this.arr[min] !== null) {
-    [this.arr[curr], this.arr[min]] = [this.arr[min], this.arr[curr]];
-    // reset curr and min
-    curr = min;
-    left = curr * 2 + 1;
-    right = curr * 2 + 2;
-    min = this.arr[left] < this.arr[right] ? left : right;
-  }
-  return minVal;
-};
-
-/**
- * Get size of heap
- * @returns {integer}
- */
-
-MinHeap.prototype.size = function () {
-  return this.arr.length;
-};
-
-/**
- * Return the minimum element from the heap
- * @returns {integer}
- */
-
-MinHeap.prototype.getMin = function () {
-  return this.arr[0];
-};
-
-/**
- * @param {number} k
- * @param {number[]} nums
- */
-var KthLargest = function(k, nums) {
-  this.k = k;
-
-  // create the heap from arr
-  this.minHeap = new MinHeap();
-  for (const num of nums) {
-    this.minHeap.insert(num);
-  }
-
-  // min size of k; so that kth largest element in top heap
-  while (this.minHeap.size() > this.k) {
-    this.minHeap.extractMin();
-  }
-
-}
-
-/**
- * @param {number} val
- * @return {number}
- */
-KthLargest.prototype.add = function(val) {
-  this.minHeap.insert(val);
-  // if heap is initialized with less than k elements
-  if (this.minHeap.size() > this.k) {
-    this.minHeap.extractMin(); // maintain the size of the heap
-  }
-  return this.minHeap.getMin();
+  // push val into nums 
+  let idx = this.values[left] < val ? 1 : 0; 
+  this.values.splice(left + idx, 0, val); 
+  return this.values[this.values.length - this.k];
 }
 
 /** 
