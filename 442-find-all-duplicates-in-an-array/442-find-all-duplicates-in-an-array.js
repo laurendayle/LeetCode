590618b37f1194876b28d43dda6ebb6f3c0e13f7
Lauren Anderson <laurendayle.anderson@gmail.com>
2022-07-15 12:24:57 -0400

    Time: 202 ms (26.09%), Space: 60.6 MB (10.03%) - LeetHub

--- 442-find-all-duplicates-in-an-array/442-find-all-duplicates-in-an-array.js
+++ 442-find-all-duplicates-in-an-array/442-find-all-duplicates-in-an-array.js
@@ -3,14 +3,14 @@
  * @return {number[]}
  */
 var findDuplicates = function(nums) {
-
-  let result = [],
-      hash = new Map(); 
-  for (var i = 0; i < nums.length; i++) {
-    if (hash.has(nums[i])) {
-      result.push(nums[i]); 
+   let hash = new Map();
+  const res = []; 
+  for (let i = 0; i < nums.length; i++) {
+    if (!hash.has(nums[i])) {
+      hash.set(nums[i], 1);
+    } else {
+      res.push(nums[i]);
     }
-    hash.set(nums[i]); 
   }
-  return result; 
-}
+  return res;
+};
