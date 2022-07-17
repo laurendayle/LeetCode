987bd291c61c1c01d1b02999d4af9f1a8c19e18e
Lauren Anderson <laurendayle.anderson@gmail.com>
2022-07-17 16:13:54 -0400

    Time: 143 ms (93.29%), Space: 53.3 MB (32.75%) - LeetHub

--- 15-3sum/15-3sum.js
+++ 15-3sum/15-3sum.js
@@ -3,34 +3,27 @@
  * @return {number[][]}
  */
 var threeSum = function(nums) {
+  if(!nums || nums.length < 3) return [];
 
-  nums.sort((a, b) => a - b);
-  const result = [];
+  nums.sort((a,b) => a-b);
+  let result = [];
 
-  for (let i = 0; i < nums.length - 2; i++) {
-    if (nums[i] !== nums[i - 1]) {
-      let left = i + 1;
-      let right = nums.length - 1;
+  for(let i=0; i<nums.length; i++) {
+      if(i > 0 && nums[i-1] === nums[i]) continue;
+      let remaining = 0 - nums[i];
+      let [left, right] = [i+1, nums.length - 1];
+      while(left < right) {
+          let sum = nums[left] + nums[right];
+          if(sum < remaining) left++;
+          else if(sum > remaining) right--;
+          else {
+              let val = [nums[i], nums[left], nums[right]];
+              result.push(val);
+              left++;
+              while(left < right && nums[left-1] === nums[left]) left++;
 
-      while (left < right) {
-        const currentSum = nums[i] + nums[left] + nums[right];
-        if (currentSum === 0) {
-          result.push([nums[i], nums[left], nums[right]]);
-          while (nums[left] === nums[left + 1]) {
-            left++;
           }
-          while (nums[right] === nums[right - 1]) {
-            right--;
-          }
-          left++;
-          right--;
-        } else if (currentSum < 0) {
-          left++;
-        } else if (currentSum > 0) {
-          right--;
-        }
       }
-    }
   }
   return result;
-}
+};
