2bba353569bbf519ee190fb9361eb8caa9938bba
Lauren Anderson <laurendayle.anderson@gmail.com>
2022-07-17 15:22:20 -0400

    Time: 85 ms (56.93%), Space: 42.1 MB (54.26%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.js
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.js
@@ -3,29 +3,22 @@
  * @param {number} target
  * @return {number}
  */
-var search = function(nums, target) {
-  let start = 0; 
-  let end = nums.length - 1; 
-  
-  while (start < end) {
-    let mid = Math.floor((start + end) / 2); 
-    
-    if (nums[mid] === target) {
-      return mid; 
-    }
-    if (nums[start] <= nums[mid]) {
-      if (target >= nums[start] && target < nums[mid]) {
-        end = mid; 
-      } else {
-        start = mid + 1; 
-      }
+const search = (nums, target) => {
+  let start = 0,
+      end = nums.length - 1;
+
+
+  while (start <= end) {
+    let mid = Math.floor(start + (end - start) / 2);
+
+    if (nums[mid] === target) return mid;
+    else if (nums[mid] >= nums[start]) {
+      if (target >= nums[start] && target < nums[mid]) end = mid - 1;
+      else start = mid + 1;
     } else {
-      if (target > nums[mid] && target <= nums[end]) {
-        start = mid + 1; 
-      } else {
-        end = mid; 
-      }
+      if (target <= nums[end] && target > nums[mid]) start = mid + 1;
+      else end = mid - 1;
     }
   }
-  return nums[start] === target ? start : -1; 
-};
+  return -1;
+}
