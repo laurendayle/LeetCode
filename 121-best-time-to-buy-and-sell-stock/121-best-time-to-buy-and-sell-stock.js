0e87e1d8a1fe06c9e9e6756192fb307adf2ae844
Lauren Anderson <laurendayle.anderson@gmail.com>
2022-07-20 19:24:29 -0400

    Time: 148 ms (22.75%), Space: 51.5 MB (78.63%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.js
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.js
@@ -2,14 +2,13 @@
  * @param {number[]} prices
  * @return {number}
  */
-var maxProfit = prices => {
-
-  let minPrice = +Infinity,
-      maxProfit = 0;
-    
-  for (var i = 0; i < prices.length; i++) {
-    minPrice = Math.min(minPrice, prices[i]);
-    maxProfit = Math.max(prices[i] - minPrice, maxProfit);
+const maxProfit = prices => {
+  let max = -Infinity,
+      min = Infinity;
+  
+  for (let i = 0; i < prices.length; i++) {
+    min = Math.min(min, prices[i]);
+    max = Math.max(max, prices[i] - min);
   }
-  return maxProfit;
+  return max; 
 }
