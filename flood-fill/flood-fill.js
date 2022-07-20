47dfdb120f6531223a231b49ed419dfff8df027e
Lauren Anderson <laurendayle.anderson@gmail.com>
2022-07-20 15:15:19 -0400

    Time: 108 ms, Memory: 44.6 MB - LeetHub

--- flood-fill/flood-fill.js
+++ flood-fill/flood-fill.js
@@ -5,21 +5,20 @@
  * @param {number} color
  * @return {number[][]}
  */
-var floodFill = (image, sr, sc, color) => {
+var floodFill = function(image, sr, sc, color) {
   if (image[sr][sc] === color) return image;
+  
   let val = image[sr][sc];
-
+  
   const fill = (image, i, j, color, val) => {
-    if (i < 0 || i >= image.length || j < 0 || j >= image[0].length || image[i][j] !== val) {
-      return;
-    }
-
+    if (i < 0 || i >= image.length || j < 0 || j >= image[0].length || image[i][j] !== val) return;
+    
     image[i][j] = color;
     fill(image, i + 1, j, color, val);
     fill(image, i - 1, j, color, val);
     fill(image, i, j + 1, color, val);
-    fill(image, i, j -1, color, val);
+    fill(image, i, j - 1, color, val);
   }
   fill(image, sr, sc, color, val);
   return image;
-}
+};
